ae774b71375caeebe8e2a7a43d3b5453557a17c3
LeonidSarkisyan <TOPsarkisyanTOP@yandex.ru>
2024-02-01 12:55:20 +0300

    prod 5

--- src/config.py
+++ src/config.py
@@ -10,7 +10,7 @@ DB_HOST = os.environ.get("DB_HOST_TEST") if DEBUG else os.environ.get("DB_HOST")
 DB_PORT = os.environ.get("DB_PORT_TEST") if DEBUG else os.environ.get("DB_PORT")
 DB_NAME = os.environ.get("DB_NAME_TEST") if DEBUG else os.environ.get("DB_NAME")
 DB_USER = os.environ.get("DB_USER_TEST") if DEBUG else os.environ.get("DB_USER")
-DB_PASS = os.environ.get("DB_PASS_TEST") if DEBUG else os.environ.get("DB_PASS")
+DB_PASS = os.environ.get("DB_PASS_TEST") if DEBUG else os.environ.get("DB_PASS").replace("\n", "").replace(" ", "")
 
 print(f"Пароль от БД = {DB_PASS}")
 
