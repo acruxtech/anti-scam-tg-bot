8f0f209c7c5a5a0448317e4e4c9568b550f54d5f
LeonidSarkisyan <TOPsarkisyanTOP@yandex.ru>
2024-02-01 13:26:53 +0300

    prod 5

--- src/handlers/basic.py
+++ src/handlers/basic.py
@@ -85,7 +85,7 @@ async def get_scammer_id(message: Message, state: FSMContext, bot: Bot):
 
 @basic_router.message(ScammerSearchState.get_scammer_username)
 async def get_scammer_username(message: Message, state: FSMContext, bot: Bot):
-    username = message.text.strip().replace("@", "")
+    username = message.text.strip().replace("@", "").replace("https://t.me/", "")
 
     scammer = await scammers_service.get_scammer_by_username(username)
 
