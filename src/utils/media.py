2e096e5e66af19b75f88e7af82429d07cfd6d2e5
LeonidSarkisyan <TOPsarkisyanTOP@yandex.ru>
2024-02-08 15:20:14 +0300

    prod 6

--- src/utils/media.py
+++ src/utils/media.py
@@ -11,11 +11,13 @@ async def create_media(scammer, proof, message: Message, bot: Bot):
     media = list(media)
     media = sorted(media, key=lambda x: x[0])
 
+    print(media)
+
     media = [ScammerMedia(
         id=item[0],
         type=item[1],
         file_id=item[2],
-        scammers_reports_id=item[3],
+        proof_id=item[3],
         scammer_id=item[4]
     ) for item in media]
 
