96b3bcf6b01cb7b1a9a790ac2423a34513013130
LeonidSarkisyan <TOPsarkisyanTOP@yandex.ru>
2024-02-20 08:59:32 +0300

    add channel observe

--- src/messages.py
+++ src/messages.py
@@ -7,7 +7,7 @@ def get_start_message(message: Message) -> str:
 
 <b>Проверяй</b> исполнителя услуг на честность <b>с помощью нашего бота.</b>
 
-<b>Узнавай больше про ТГ:</b> @keRoy / <a href="https://t.me/+PXCfc4FyhMVjMzQ6">Rakhimov ВЕЩАЕТ</a>
+<b>Узнавай больше про ТГ:</b> <a href="https://t.me/+qyKIFJSJ0DwzZmUy">@keRoy</a> / <a href="https://t.me/+PXCfc4FyhMVjMzQ6">Rakhimov ВЕЩАЕТ</a>
 
 <b>Агентство</b> – <a href="https://t.me/spaceagency1">Space Agency</a>
 
